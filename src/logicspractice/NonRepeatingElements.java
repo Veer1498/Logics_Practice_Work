2aa187b238b2529120b352a23431ceef68622922
Veer1498 <singavarapuveerendra@gmail.com>
2022-08-04 22:01:32 +0530

    Added : Welcome Message

---
+++ src/logicspractice/NonRepeatingElements.java
@@ -0,0 +1,19 @@
+package logicspractice;
+import java.util.Scanner;
+/**
+ * 
+ * @author Veer.Singa
+ *
+ */
+public class NonRepeatingElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Repeating Elements on Array Program");
+		System.out.println("**********************************************");
+		
+		
+		
+	}
+
+}
